83f355f821efdfb8d9a1777b4215719b7bf2566c
bassz <bassgamer2016@gmail.com>
2026-05-07 20:55:08 +0700

    add more js practice project.

---
+++ JS PRACTICE PROJECTS/Javascript Trivia Bot/script.js
@@ -0,0 +1,21 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+const botName = "Opus";
+const botLocation = "Thailand";
+const favoriteLanguage = "JS";
+
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+let codingFact = favoriteLanguage + " was created in just 10 days by Brendan Eich in 1995.";
+
+console.log(codingFact);
+
+codingFact = favoriteLanguage + " is the only programming language that runs natively in web browsers.";
+
+console.log(codingFact);
+
+codingFact = favoriteLanguage + " is the most popular programming language in the world for over 10 years in a row.";
+
+console.log(codingFact);
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".");
