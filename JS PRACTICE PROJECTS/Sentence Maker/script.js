60403bc08487df6c40549fbb3e54c13f11632419
bassz <bassgamer2016@gmail.com>
2026-05-08 00:48:34 +0700

    add sectence maker js practice project.

---
+++ JS PRACTICE PROJECTS/Sentence Maker/script.js
@@ -0,0 +1,21 @@
+let adjective = "brave";
+let noun = "knight";
+let verb = "discovered";
+let place = "forest";
+let adjective2 = "anclent";
+let noun2 = "treasure";
+
+const firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 +". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+console.log("First story: " + firstStory);
+
+adjective = "tiny";
+noun = "Robot";
+verb = "explored";
+place = "moon";
+adjective2 = "mysterious";
+noun2 = "crystal";
+
+const secondStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 +". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+console.log("Second story: " + secondStory);
