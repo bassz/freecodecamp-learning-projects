a453fa5f9f6ff4f7162dd3c14dd158cf612ee892
bass <bassgamer2016@gmail.com>
2026-05-23 02:16:11 +0700

    fix function in lunch picker program

--- JS PRACTICE PROJECTS/Lunch Picker Program/script.js
+++ JS PRACTICE PROJECTS/Lunch Picker Program/script.js
@@ -1,5 +1,30 @@
 const lunches = [];
 function addLunchToEnd(lunches, str) {
   lunches.push(str);
-  return console.log(`${lunches}`)
+  console.log(`${str} added to the end of the lunch menu.`);
+  return lunches;
+}
+
+function addLunchToStart(lunches, str) {
+  lunches.unshift(str);
+  console.log(`${str} added to the start of the lunch menu.`);
+  return lunches;
+}
+
+function removeLastLunch(lunches) {
+  lunches.pop();
+  if (lunches = "") {
+    return console.log("No lunches to remove.");
+  } else {
+    return console.log(`${lunches} removed from the end of the lunch menu.`);
+  }
+}
+
+function removeFirstLunch(lunches) {
+  lunches.shift();
+  if (lunches.shift() === undefined) {
+    return console.log("No lunches to remove.");
+  } else {
+    return console.log(`${lunches} remremoved from the start of the lunch menu.`);
+  }
 }
