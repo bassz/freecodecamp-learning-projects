4e6c8d6f8b71810c6fe40d3c2c63490dba7bd5ef
bass <bassgamer2016@gmail.com>
2026-05-24 19:42:32 +0700

    fix something

--- JS PRACTICE PROJECTS/Lunch Picker Program/script.js
+++ JS PRACTICE PROJECTS/Lunch Picker Program/script.js
@@ -1,9 +1,11 @@
-const lunches = [];
+let lunches = [];
 function addLunchToEnd(lunches, str) {
   lunches.push(str);
   console.log(`${str} added to the end of the lunch menu.`);
   return lunches;
 }
+
+addLunchToEnd(["Pizza", "Tacos"], "Burger");
 
 function addLunchToStart(lunches, str) {
   lunches.unshift(str);
@@ -12,19 +14,42 @@ function addLunchToStart(lunches, str) {
 }
 
 function removeLastLunch(lunches) {
-  lunches.pop();
-  if (lunches = "") {
-    return console.log("No lunches to remove.");
+  if (lunches.length === 0) {
+    console.log("No lunches to remove.");
+    return lunches;
   } else {
-    return console.log(`${lunches} removed from the end of the lunch menu.`);
+    console.log(`${lunches.pop()} removed from the end of the lunch menu.`);
+    return lunches;
   }
 }
 
+
+const result = removeLastLunch(["Stew", "Soup", "Toast"]);
+console.log(result); //อันนี้เป็นการลองใช้ array.pop() ที่จะคืนค่า lunches หลังจากตัดตัวหลังสุดออก
+
 function removeFirstLunch(lunches) {
-  lunches.shift();
-  if (lunches.shift() === undefined) {
-    return console.log("No lunches to remove.");
+  if (lunches.length === 0) {
+    console.log("No lunches to remove.");
+    return lunches;
   } else {
-    return console.log(`${lunches} remremoved from the start of the lunch menu.`);
+    console.log(`${lunches.shift()} removed from the start of the lunch menu.`);
+    return lunches;
   }
 }
+removeFirstLunch(["Sushi", "Pizza", "Burger"]);
+
+function getRandomLunch(lunches) {
+  if (lunches.length === 0) {
+    console.log("No lunches available.");
+    return lunches;
+  } else {
+    console.log(`Randomly selected lunch: ${lunches}`);
+    return lunches
+  }
+}
+
+
+const min = 1;
+const max = 100;
+const randomNum2 = Math.random() * (max - min) + min;
+console.log(randomNum2);
