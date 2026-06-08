719ccc4fd36ef630110d0abec2dc036ec8d32866
bassz <bassgamer2016@gmail.com>
2026-06-08 16:47:46 +0700

    add JS DAILY CODING CHALLENGES and add project

---
+++ JS Daily Coding Challenge/script.js
@@ -0,0 +1,24 @@
+function lastLoadDate(scoops, usage) {
+  return Math.floor(scoops / (usage.reduce((acc, cur) => acc + cur, 0) / usage.length));
+}
+
+console.log(lastLoadDate(10, [2, 2, 2, 2, 2, 2, 2]));
+
+/*
+โจทย์:
+Given the number of scoops of laundry detergent you have remaining and an array of how many scoops you used in each of the previous days, return the number of full days of detergent you have remaining.
+
+Calculate your average daily usage from the usage history and assume that amount of usage each day going forward.
+
+
+-------------------------
+test:
+
+lastLoadDate(16, [2, 3, 0, 3, 4, 2, 1]) should return 7.
+
+lastLoadDate(33, [5, 0, 4, 3, 3, 2]) should return 11.
+
+lastLoadDate(50, [2, 0, 2, 9, 12, 0, 2]) should return 12.
+
+lastLoadDate(20, [13, 9, 12, 10, 8]) should return 1.
+*/
