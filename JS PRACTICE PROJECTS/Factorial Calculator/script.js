691188bc07b823a5347131db7d7e0ae4b47a6916
bassz <bassgamer2016@gmail.com>
2026-06-22 01:30:15 +0700

    add more js practice project (factorial calculator)

---
+++ JS PRACTICE PROJECTS/Factorial Calculator/script.js
@@ -0,0 +1,12 @@
+const num = 5;
+function factorialCalculator(n) {
+  let result = 1;
+  for (let i = 1; i <= n; i++) {
+    result = result * i;
+  }
+  return result;
+}
+
+const factorial = factorialCalculator(num);
+const resultMsg = `Factorial of ${num} is ${factorial}`;
+console.log(resultMsg);
