e1a8b4aeaf37e603043a8fd75dd1674cca8ea864
bassz <bassgamer2016@gmail.com>
2026-06-23 00:18:10 +0700

    add more js practice project (chunky monkey algorithm)

---
+++ JS PRACTICE PROJECTS/Chunky Monkey Algorithm/script.js
@@ -0,0 +1,18 @@
+function chunkArrayInGroups(arr, size) {
+  const result = [];
+  for (let i = 0; i < arr.length; i += size) {
+    result.push(arr.slice(i, i + size));
+  }
+  return result;
+}
+
+console.log(chunkArrayInGroups(["a", "b", "c", "d"], 2));
+
+/*
+ตัวอย่างอื่น:
+console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5], 3));
+// → [[0, 1, 2], [3, 4, 5]]
+
+console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6], 3));
+// → [[0, 1, 2], [3, 4, 5], [6]]
+*/
